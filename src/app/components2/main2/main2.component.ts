d7a899f4d954c142500b080e6017997552642d35
Evanwood5 <evan.wood861@gmail.com>
2024-10-11 13:53:50 -0400

    Updated TypeScript, CSS, and HTML files for better layout and functionality

--- src/app/components2/main2/main2.component.ts
+++ src/app/components2/main2/main2.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { Header2Component } from '../header2/header2.component';
 import { ContactComponent } from '../contact/contact.component';
-import { Start2Component } from "../start2/start2.component";
-
+import { Start2Component } from '../start2/start2.component';
 import { Mobile2Component } from '../mobile2/mobile2.component';
 
 @Component({
@@ -10,8 +11,16 @@ import { Mobile2Component } from '../mobile2/mobile2.component';
   standalone: true,
   imports: [Header2Component, ContactComponent, Start2Component, Mobile2Component],
   templateUrl: './main2.component.html',
-  styleUrl: './main2.component.css'
+  styleUrls: ['./main2.component.css'] // Fixed typo from styleUrl to styleUrls
 })
-export class Main2Component {
+export class Main2Component implements OnInit {
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      window.scrollTo(0, 0); // Scroll to top of the page
+    });
+  }
 }
--- src/app/components2/main3/main3.component.ts
+++ src/app/components2/main3/main3.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { Header2Component } from '../header2/header2.component';
 import { Contact2Component } from '../contact2/contact2.component';
-import { PricingComponent } from "../pricing/pricing.component";
+import { PricingComponent } from '../pricing/pricing.component';
 import { Mobile2Component } from '../mobile2/mobile2.component';
-
-
 
 @Component({
   selector: 'app-main3',
   standalone: true,
-  imports: [Header2Component, Contact2Component, PricingComponent,Mobile2Component],
+  imports: [Header2Component, Contact2Component, PricingComponent, Mobile2Component],
   templateUrl: './main3.component.html',
-  styleUrl: './main3.component.css'
+  styleUrls: ['./main3.component.css']
 })
-export class Main3Component {
+export class Main3Component implements OnInit {
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      window.scrollTo(0, 0); // Scroll to top of the page
+    });
+  }
 }
