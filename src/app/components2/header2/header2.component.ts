09171b03005783afded1ff43081baa85c3d1c5a7
Evanwood5 <evan.wood861@gmail.com>
2024-11-05 15:35:52 -0500

    added new page icon and updated scroll for screen 2

--- src/app/components2/header2/header2.component.ts
+++ src/app/components2/header2/header2.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-header2',
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './header2.component.html',
   styleUrl: './header2.component.css'
 })
-export class Header2Component {
+export class Header2Component implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    const header = document.querySelector('.header') as HTMLElement; // Explicitly cast header as HTMLElement
+
+    if (header) {
+        const stickyClass: string = 'sticky'; // Explicitly declare the type as string
+
+        window.addEventListener('scroll', () => {
+            console.log("Scroll position:", window.scrollY);
+            if (window.scrollY > header.offsetHeight) {
+                header.classList.add(stickyClass);
+                console.log("Sticky class added");
+            } else {
+                header.classList.remove(stickyClass);
+                console.log("Sticky class removed");
+            }
+        });
+    }
+  }
 }
